958256be50aeffa9604fecd05f369a158330e46d
John Ellmore <git@johnellmore.com>
2024-02-20 12:06:15 -0600

    clean fe dir

--- scripts/buildClient.js
+++ scripts/buildClient.js
@@ -1,5 +1,13 @@
+const fs = require("fs");
+const path = require("path");
 const esbuild = require("esbuild");
 const manifestPlugin = require("esbuild-plugin-manifest");
+
+// clean the `dist/static` directory first
+const distStatic = path.join(__dirname, "..", "dist", "static");
+if (fs.existsSync(distStatic)) {
+  fs.rmSync(distStatic, { recursive: true });
+}
 
 const opts = {
   entryPoints: ["src/client.tsx"],
